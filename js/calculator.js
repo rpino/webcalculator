dca9a0863a25a82894b5658a3a1c6791a8962624
rpino <pino.islas@gmail.com>
2024-12-28 23:43:13 -0800

    Implementation of some logic on buttons, still need to work on the right display of the inputs

---
+++ js/calculator.js
@@ -0,0 +1,16 @@
+function getInput(input){
+    alert(input);
+}
+
+
+function displayValue(input){
+    let display = document.getElementById('display');
+}
+
+function calculate(){
+    alert('Calculate');
+}
+
+function clearDisplay(){
+    display = '';
+}   
